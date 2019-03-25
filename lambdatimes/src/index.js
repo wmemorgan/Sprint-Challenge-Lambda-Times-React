fbf0132f56d56680d827cc88a08afb1aaac9d53b
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-03-25 22:50:08 +0800

    Complete CSS migration to styled components

--- lambdatimes/src/index.js
+++ lambdatimes/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './CSS/index.css';
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle from './components/StyleComponents/GlobalStyle'
 import theme from './components/StyleComponents/theme'
@@ -15,7 +14,6 @@ const Root = () => {
         <App />
       </>
     </ThemeProvider>
-
   )
 }
 
