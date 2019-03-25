f9d7804275c96a24945c199b139013e1bc18f3da
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-03-25 14:24:47 +0800

    Add carousel navigation functionality

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -9,26 +9,46 @@ export default class Carousel extends Component {
       selection: 0
     }
   }
+  // Load carousel data into state
   componentDidMount() {
     this.setState({ data: carouselData })
   }
-
+  // Go back to previous hero image
   leftClick = () => {
-
+    this.setState(prevState => (
+      {
+        selection: prevState.selection - 1 < 0 ? 
+        prevState.data.length - 1 : 
+        prevState.selection - 1
+      }
+    ))
   }
-
+ // Advance to next hero image
   rightClick = () => {
-
+    this.setState(prevState => (
+      {
+        selection: prevState.selection + 1 >= prevState.data.length ?
+        0 : prevState.selection + 1
+      }
+    ))
   }
 
+  // Display hero image
   selectedImage = () => {
     const { data, selection } = this.state
-    return <img src={data[selection]} style={{ display: 'block' }} />
+    return <img src={data[selection]} alt='hero banner' style={{ display: 'block' }} />
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.selection !== this.state.selection) {
+      this.selectedImage()
+    }
   }
 
   render() {
     return (
       <div className="carousel">
+        {this.selectedImage()}
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
